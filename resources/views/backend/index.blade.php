b7556d087de06de7b97ec20e35c37b2f4fafc065
ddeshar <jedeshar@gmail.com>
2018-10-12 21:23:45 +0700

    added backend Theme

---
+++ resources/views/backend/index.blade.php
@@ -0,0 +1,19 @@
+@extends('backend._layouts.master')
+
+@section('header_styles')
+
+@endsection
+
+@section('content')
+  <div class="row">
+    <div class="col-md-12">
+      <div class="tile">
+        <div class="tile-body">Create a beautiful dashboard</div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('footer_scripts')
+
+@endsection
