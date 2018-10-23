114c3ee680ecf45abf574ca41ce444b2bd2f5a5c
ddeshar <jedeshar@gmail.com>
2018-10-23 13:11:12 +0700

    added dynamic menu sub menu not working yet

--- resources/views/backend/_layouts/_partial/_sideMenu.blade.php
+++ resources/views/backend/_layouts/_partial/_sideMenu.blade.php
@@ -7,7 +7,7 @@
         </div>
     </div>
     <ul class="app-menu">
-        <li><a class="app-menu__item active" href="{{ url('/') }}"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+        <!-- <li><a class="app-menu__item active" href="{{ url('/') }}"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Pages</span><i class="treeview-indicator fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
                 <li><a class="treeview-item" href="{{ url('/') }}"><i class="icon fa fa-circle-o"></i> Blank Page</a></li>
@@ -15,6 +15,16 @@
                 <li><a class="treeview-item" href="{{ url('/') }}"><i class="icon fa fa-circle-o"></i> Lockscreen Page</a></li>
                 <li><a class="treeview-item" href="{{ url('/') }}"><i class="icon fa fa-circle-o"></i> User Page</a></li>
             </ul>
-        </li>
+        </li> -->
+
+        @foreach(Wocmenu::GenerateMenu(2) as $key => $category)
+            <li><a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">{{ $category->title_th }}</span></a>
+                <!-- @if(count($category->childs))
+                    @include('backend._layouts._includes.manageChild',['childs' => $category->childs])
+                @endif -->
+            </li>
+        @endforeach
+
+
     </ul>
 </aside>
