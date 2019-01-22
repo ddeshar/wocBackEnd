f64b9120eb096d8c1710e48a7eef8e35890acbbf
ddeshar <jedeshar@gmail.com>
2019-01-22 16:06:09 +0700

    added categoty

---
+++ resources/views/backend/categories/create.blade.php
@@ -0,0 +1,24 @@
+@extends('backend._layouts.master')
+
+@section('title', '')
+
+@section('header_styles')
+
+@endsection
+
+@section('content')
+    {!! Form::open([ 'route' => 'categories.store', 'method' => 'POST']) !!}
+        @include('backend.categories._form')
+    {!! Form::close() !!}
+@endsection
+
+@section('footer_scripts')
+<script type="text/javascript">    
+    $("#title").keyup(function(){
+        var str = $(this).val();
+        var trims = $.trim(str)
+        var slug = trims.replace(/[^a-z0-9]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '')
+        $("#slug").val(slug.toLowerCase())
+    })
+</script>
+@endsection
---
+++ resources/views/backend/categories/index.blade.php
@@ -0,0 +1,58 @@
+@extends('backend._layouts.master')
+
+@section('title', '')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tile">
+                <div class="tile-title-w-btn">
+                    <h3 class="title">categories</h3>
+                    <p><a class="btn btn-primary icon-btn" href="{{ route('categories.create') }}"><i class="fa fa-plus"></i>Add categories</a></p>
+                </div>
+
+                <div class="tile-body">
+                    <table class="table table-hover table-bordered" id="sampleTable">
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>order</th>
+                                <th>name</th>
+                                <th>slug</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $i = 0 ?>
+                            @foreach ($categories as $categorie)
+                                <tr>
+                                    <?php $i++ ?>
+                                    <td>{{ $i }}</td>
+                                    <td>{{ $categorie->order }}</td>
+                                    <td>{{ $categorie->name }}</td>
+                                    <td>{{ $categorie->slug }}</td>
+                                    <td>
+                                        <div class="btn-group pull-right">
+                                            <a class="btn btn-primary" href="{{ route('categories.show', $categorie->id) }}"><i class="fa fa-eye"></i></a>
+                                            <a class="btn btn-warning" href="{{ route('categories.edit', $categorie->id) }}"><i class="fa fa-edit"></i></a>
+                                            {!! Form::open(['method' => 'DELETE','route' => ['categories.destroy', $categorie->id],'style'=>'display:inline']) !!}
+                                                {{ Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger'] )  }}
+                                            {!! Form::close() !!}
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach                    
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('footer_scripts')
+    <!-- Data table plugin-->
+    <script type="text/javascript" src="{{ asset('js/plugins/jquery.dataTables.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/plugins/dataTables.bootstrap.min.js') }}"></script>
+    <script type="text/javascript">$('#sampleTable').DataTable();</script>
+@endsection
